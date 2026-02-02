b31c3ed06c4daa1f7025289ba7fc5323588fd906
Geodayo <george.pikachu@gmail.com>
2026-02-02 01:27:31 -0500

    second commit

---
+++ assets/balloons.stories-BVR-6AWm.js
@@ -0,0 +1,8 @@
+import{x as i}from"./iframe-DtELfyQx.js";import"./preload-helper-PPVm8Dsz.js";const a={title:"Components/Balloons",render:s=>i`
+    <vh-microsite-balloons .images=${s.images} .total=${s.total}></vh-microsite-balloons>
+  `},e={args:{total:67,images:["/images/vh-microsite-b1.png","/images/vh-microsite-b2.png","/images/vh-microsite-b3.png","/images/vh-microsite-b4.png"]}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    total: 67,
+    images: ["/images/vh-microsite-b1.png", "/images/vh-microsite-b2.png", "/images/vh-microsite-b3.png", "/images/vh-microsite-b4.png"]
+  }
+}`,...e.parameters?.docs?.source}}};const m=["Default"];export{e as Default,m as __namedExportsOrder,a as default};
---
+++ assets/participate.stories-CxM1pSIc.js
@@ -0,0 +1,24 @@
+import{x as n}from"./iframe-DtELfyQx.js";import"./preload-helper-PPVm8Dsz.js";const r={title:"Components/Participate",render:t=>n`
+    <vh-microsite-participate
+      heading="${t.heading}"
+      date="${t.date}"
+      .steps="${t.steps}"
+      subtext="${t.subtext}"
+    ></vh-microsite-participate>
+  `},e={args:{heading:"How to <strong>Participate</strong>",date:"From February 19, 2026 to April 15, 2026",steps:[{step:"01",text:"<strong>Follow</strong> us on Instagram and like our #Since1992 post."},{step:"02",text:"<strong>Tag</strong> two friends in the comments for a chance to win a Vintage Gift Pack."},{step:"Bonus",text:"<strong>Share</strong> your own “Then & Now” post or story, add #Since1992, and tag @VibrantHealth to triple your chances."}],subtext:"<p>We’ll pick weekly VH retro gift package winners and share our favorites. 💚 Tag two friends that might also want to win Vibrant Health Vintage Gift Pack.</p>"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    heading: "How to <strong>Participate</strong>",
+    date: "From February 19, 2026 to April 15, 2026",
+    steps: [{
+      step: "01",
+      text: "<strong>Follow</strong> us on Instagram and like our #Since1992 post."
+    }, {
+      step: "02",
+      text: "<strong>Tag</strong> two friends in the comments for a chance to win a Vintage Gift Pack."
+    }, {
+      step: "Bonus",
+      text: "<strong>Share</strong> your own “Then & Now” post or story, add #Since1992, and tag @VibrantHealth to triple your chances."
+    }],
+    subtext: "<p>We’ll pick weekly VH retro gift package winners and share our favorites. 💚 Tag two friends that might also want to win Vibrant Health Vintage Gift Pack.</p>"
+  }
+}`,...e.parameters?.docs?.source}}};const s=["Default"];export{e as Default,s as __namedExportsOrder,r as default};
