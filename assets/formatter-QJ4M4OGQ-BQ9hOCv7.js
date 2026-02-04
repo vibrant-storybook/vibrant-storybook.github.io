32bf38eec7d6dfaa5a7555b90846cb84856b42cd
Geodayo <george.pikachu@gmail.com>
2026-02-04 07:43:27 -0500

    latest 2426

--- assets/formatter-QJ4M4OGQ-BQ9hOCv7.js
+++
@@ -1,1 +0,0 @@
-import{d as m,b as e}from"./DocsRenderer-GHJI37HO-DsmUyRSp.js";import{O as o}from"./iframe-D76UhJot.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
--- assets/gallery.stories-BsHrh9au.js
+++
@@ -1,9 +0,0 @@
-import{x as e}from"./iframe-D76UhJot.js";import"./preload-helper-PPVm8Dsz.js";const r={title:"Components/Gallery",render:i=>e`
-    <vh-microsite-gallery
-            .items=${i.items}
-    ></vh-microsite-gallery>
-  `},s={args:{items:["/images/vh-microsite-slideshow-0.png","/images/vh-microsite-slideshow-1.png","/images/vh-microsite-slideshow-2.png","/images/vh-microsite-slideshow-3.png","/images/vh-microsite-slideshow-4.png","/images/vh-microsite-slideshow-5.png","/images/vh-microsite-slideshow-6.png","/images/vh-microsite-slideshow-7.png","/images/vh-microsite-slideshow-8.png","/images/vh-microsite-slideshow-9.png"]}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
-  args: {
-    items: ["/images/vh-microsite-slideshow-0.png", "/images/vh-microsite-slideshow-1.png", "/images/vh-microsite-slideshow-2.png", "/images/vh-microsite-slideshow-3.png", "/images/vh-microsite-slideshow-4.png", "/images/vh-microsite-slideshow-5.png", "/images/vh-microsite-slideshow-6.png", "/images/vh-microsite-slideshow-7.png", "/images/vh-microsite-slideshow-8.png", "/images/vh-microsite-slideshow-9.png"]
-  }
-}`,...s.parameters?.docs?.source}}};const g=["Default"];export{s as Default,g as __namedExportsOrder,r as default};
