fc6943eba2ce24237e6b20efe8e8ca5ea6cdae8d
Geodayo <george.pikachu@gmail.com>
2026-03-13 21:22:26 -0400

    latest 31326

---
+++ assets/circle-marquee.stories-BgPjv1iS.js
@@ -0,0 +1,28 @@
+import{b as t}from"./iframe-DZTSBM7b.js";import"./preload-helper-PPVm8Dsz.js";const n={title:"Components/Circle Marquee",render:r=>t`
+    <vh-microsite-circle-marquee
+      .heading=${r.heading}
+      .subtitle=${r.subtitle}
+      .items=${r.items}
+    ></vh-microsite-circle-marquee>
+  `},e={args:{heading:"Transparency Grows with <strong>the Company We Keep</strong>",subtitle:"Good science starts with good people—and we work with the best.",items:[{src:"/images/circle-marquee-1.png",alt:"JIAHERB"},{src:"/images/circle-marquee-2.png",alt:"USGreens"},{src:"/images/circle-marquee-3.png",alt:"LIQUADRY"},{src:"/images/circle-marquee-4.png",alt:"Future Ceuticals"},{src:"/images/circle-marquee-5.png",alt:"Seatech"}]}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    heading: "Transparency Grows with <strong>the Company We Keep</strong>",
+    subtitle: "Good science starts with good people\\u2014and we work with the best.",
+    items: [{
+      src: "/images/circle-marquee-1.png",
+      alt: "JIAHERB"
+    }, {
+      src: "/images/circle-marquee-2.png",
+      alt: "USGreens"
+    }, {
+      src: "/images/circle-marquee-3.png",
+      alt: "LIQUADRY"
+    }, {
+      src: "/images/circle-marquee-4.png",
+      alt: "Future Ceuticals"
+    }, {
+      src: "/images/circle-marquee-5.png",
+      alt: "Seatech"
+    }]
+  }
+}`,...e.parameters?.docs?.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,n as default};
